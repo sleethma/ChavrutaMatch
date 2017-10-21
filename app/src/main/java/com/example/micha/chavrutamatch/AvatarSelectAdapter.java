dc048e94d4c4307920342da6351be87a54dcf794
Michael Sleeth <sleeth.ma@gmail.com>
2017-10-21 16:07:21 -0500

    index on avatar-fragment: 734310b fixes bug in app flow if user has an account but logs in a new device, profile downloads and persists from db

---
+++ app/src/main/java/com/example/micha/chavrutamatch/AvatarSelectAdapter.java
@@ -0,0 +1,8 @@
+package com.example.micha.chavrutamatch;
+
+/**
+ * Created by micha on 10/15/2017.
+ */
+
+public class AvatarSelectAdapter {
+}
---
+++ app/src/main/java/com/example/micha/chavrutamatch/Data/AvatarImgs.java
@@ -0,0 +1,8 @@
+package com.example.micha.chavrutamatch.Data;
+
+/**
+ * Created by micha on 10/15/2017.
+ */
+
+public class AvatarImgs {
+}
